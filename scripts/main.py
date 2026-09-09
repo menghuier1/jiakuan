58da09bf768d80fa171e5224cb4cfdd2e9c7ce1a
menghuier1 <menghuier1@proton.me>
2026-09-09 14:10:38 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -19,7 +19,7 @@ from datetime import datetime, timezone
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 SOURCE_URLS = [
-    "https://github.com/sakha1370/OpenRay/raw/refs/heads/main/output/all_valid_proxies.txt",
+    "https://raw.githubusercontent.com/teknovpnhub/v2ray-subscription/refs/heads/main/servers.txt",
 ]
 
 OUTPUT_DIR = "output"
